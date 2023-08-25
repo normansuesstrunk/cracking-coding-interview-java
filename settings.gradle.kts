4140f168d4a3c2581f9122025f077326fab91a8e
Norman Suesstrunk <norman.suesstrunk@fhgr.ch>
2023-08-25 14:05:42 +0200

    adjusted gradle project name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,5 +10,5 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
 }
 
-rootProject.name = "[3~"
+rootProject.name = "Cracking the Coding Interview Java"
 include("app")
